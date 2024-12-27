45461928b4b3ae3d7a773e9739c12bac425a5967
QuangHieu <quanghieupc152002@gmail.com>
2024-12-27 16:12:11 +0700

    commit n

--- app/(root)/(routes)/(dashboard)/[deviceId]/page.tsx
+++ app/(root)/(routes)/(dashboard)/[deviceId]/page.tsx
@@ -291,11 +291,11 @@ const DashboardPage = () => {
               className="px-4 py-2 text-sm bg-gray-900 text-white rounded hover:bg-gray-800"
               onClick={() =>
                 onClickDevice({
-                  buzzer: attribute?.["buzzer"] == "true" ? "false" : "true",
+                  buzzer: attribute?.["buzzer"] == true ? false : true,
                 })
               }
             >
-              {attribute?.["buzzer"] == "true" ? "Tắt" : "Bật"}
+              {attribute?.["buzzer"] == true ? "Tắt" : "Bật"}
             </Button>
           </div>
         </LatestTelemetryCard>
